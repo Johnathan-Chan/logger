26ce12d5d95934a0cd597ee039b6b9b6bcaeac2a
zhesha <zhesha@qingmutec.com>
2021-11-30 10:24:14 +0800

    add console config

--- logger.go
+++ logger.go
@@ -16,20 +16,20 @@ type Config struct {
 	MaxBackups   int
 	MaxAge       int
 	Compress     bool
+	Console      bool
 }
 
 var Hierarchical = map[string]zapcore.Level{
-	"info": zapcore.InfoLevel,
-	"warn": zapcore.WarnLevel,
-	"error": zapcore.ErrorLevel,
+	"info":   zapcore.InfoLevel,
+	"warn":   zapcore.WarnLevel,
+	"error":  zapcore.ErrorLevel,
 	"dpanic": zapcore.DPanicLevel,
-	"panic": zapcore.PanicLevel,
+	"panic":  zapcore.PanicLevel,
 	//"fatal": zapcore.FatalLevel,
 }
 
-
 type Log struct {
-	Config Config
+	Config  Config
 	Encoder zapcore.EncoderConfig
 	*zap.Logger
 }
@@ -43,19 +43,26 @@ func NewLogger(config Config) *Log {
 	return log
 }
 
-func (l *Log) InitLog () *zap.Logger{
+func (l *Log) InitLog() *zap.Logger {
 	l.GetEncoder()
 
 	if !l.Config.Hierarchical {
+		write := []zapcore.WriteSyncer{
+			l.GetLogWriter(),
+		}
+
+		if l.Config.Console {
+			write = append(write, os.Stdout)
+		}
+
 		core := zapcore.NewCore(zapcore.NewJSONEncoder(l.Encoder),
-			zapcore.NewMultiWriteSyncer(l.GetLogWriter(), os.Stdout), zap.DebugLevel)
+			zapcore.NewMultiWriteSyncer(write...), zap.DebugLevel)
 
 		return zap.New(core, zap.AddStacktrace(zap.DebugLevel))
 	}
 
-
 	cores := make([]zapcore.Core, 0)
-	for level, hierarchical := range Hierarchical{
+	for level, hierarchical := range Hierarchical {
 		l.Config.Filename = level + "/" + l.Config.Filename
 		core := zapcore.NewCore(zapcore.NewJSONEncoder(l.Encoder), l.GetLogWriter(), zap.LevelEnablerFunc(func(level zapcore.Level) bool {
 			return level == hierarchical
@@ -67,8 +74,10 @@ func (l *Log) InitLog () *zap.Logger{
 		return level == zapcore.FatalLevel
 	})))
 
-	cores = append(cores, zapcore.NewCore(zapcore.NewJSONEncoder(l.Encoder),
-		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), zapcore.DebugLevel))
+	if l.Config.Console {
+		cores = append(cores, zapcore.NewCore(zapcore.NewJSONEncoder(l.Encoder),
+			zapcore.AddSync(os.Stdout), zapcore.DebugLevel))
+	}
 
 	core := zapcore.NewTee(cores...)
 
@@ -79,7 +88,7 @@ func (l *Log) InitLog () *zap.Logger{
 	return zap.New(core)
 }
 
-func(l *Log) GetEncoder() {
+func (l *Log) GetEncoder() {
 	l.Encoder = zap.NewProductionEncoderConfig()
 	l.Encoder.TimeKey = "timestamp"
 	l.Encoder.LevelKey = "level"
@@ -92,7 +101,7 @@ func(l *Log) GetEncoder() {
 	l.Encoder.EncodeLevel = zapcore.CapitalLevelEncoder
 }
 
-func(l *Log) GetLogWriter() zapcore.WriteSyncer {
+func (l *Log) GetLogWriter() zapcore.WriteSyncer {
 	hook := Logger{
 		Dir:        l.Config.Dir,
 		Filename:   l.Config.Filename,
